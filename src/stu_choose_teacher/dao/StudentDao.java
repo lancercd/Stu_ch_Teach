de8a9ad933e9f44f677b420d06f98366688f46aa
67catl <2235096287@qq.com>
2021-01-03 23:15:23 +0800

    update StudentDao.java

--- src/stu_choose_teacher/dao/StudentDao.java
+++ src/stu_choose_teacher/dao/StudentDao.java
@@ -2,6 +2,7 @@ package stu_choose_teacher.dao;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import stu_choose_teacher.domain.GuideAdviser;
 import stu_choose_teacher.domain.Student;
 import stu_choose_teacher.utils.JDBCUtils;
@@ -11,6 +12,11 @@ import java.util.List;
 public class StudentDao {
     private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
 
+    /**
+     * 根据导师id 在导师学生表中 查询 已经选择了的学生信息
+     * @param guide_adviser_id 指导老师id
+     * @return 学生信息
+     */
     public List<Student> getStudentsByGuide(int guide_adviser_id){
         String sql = "select tb_jk2_tutor_stu.stu_id, stu_number, stu_name, stu_password \n" +
                 "from tb_jk2_tutor_stu \n" +
@@ -27,8 +33,8 @@ public class StudentDao {
 
     /**
      * 根据学生id查找学生信息
-     * @param stu_id
-     * @return
+     * @param stu_id 学生id
+     * @return 学生信息
      */
     public Student getStudent(int stu_id){
         String sql = "select * from tb_jk2_student where stu_id = ?";
@@ -40,4 +46,32 @@ public class StudentDao {
         return student;
     }
 
+    /**
+     *
+     * @param guide_adviser_id 指导老师id
+     * @return
+     */
+    public boolean checkStudentByGuide(int guide_adviser_id, int stu_id){
+        String sql = "select count(*) as row_count from tb_jk2_tutor_stu\n" +
+                "where stu_id = ? and guide_adviser_id = ?;";
+
+        SqlRowSet count = template.queryForRowSet(sql,
+                stu_id, guide_adviser_id);
+
+        int row_count = 0;
+        if(count.next()){
+            row_count = count.getInt("row_count");
+        }
+
+        if(row_count == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean b = new StudentDao().checkStudentByGuide(1, 1);
+        System.out.println(b);
+    }
 }
